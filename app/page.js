9b505b7940a1e52334e8fb2c45574e61cfda1387
Asistenteplaneacion <apoyo.planeacion@fadp.edu.co>
2024-04-05 16:18:53 -0500

    first commit

--- app/page.js
+++ app/page.js
@@ -1,95 +1,190 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+'use client'
+
+import { useEffect, useState } from "react";
+import styles from '../styles/home.css'
+// import Document from './document/page'
+
+const baseUrl = process.env.NEXT_PUBLIC_BACKEND_URL;
 
 export default function Home() {
+  const [selectedProductId, setSelectedProductId] = useState();
+  const [datas, setDatas] = useState([]);
+
+  const fetchDatas = () => {
+    fetch(`${baseUrl}/datas`)
+      .then((res) => res.json())
+      .then(({ datas }) => {
+        setDatas(datas);
+      });
+  };
+
+  useEffect(() => {
+    fetchDatas();
+  }, []);
+
+  console.log({ datas })
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
+    <>
+      <div className="container df jcsb">
+        <div className="df fdc">
+          <h2 style={{ margin: "0.3rem"}}>
+            <u>Crear nuevo producto</u>
+          </h2>
+          <form>
+            <input 
+              type="text"
+              name="name"
+              placeholder="ingrese algo"
             />
-          </a>
+          </form> 
+        </div>
+
+        <div className='products-container'>
+          {datas.map(({ _id, megas }) => (
+            <div 
+              onClick={() => setSelectedProductId(_id)}
+              key={_id}
+              className='shadow df aic jcsb p5 mb5 br5'
+              style={{
+                backgroundColor:
+                  selectedProductId === _id ? 'lightblue' : 'white',
+                width: '100',
+              }}
+            >
+              <span>{megas}</span>
+              <div className=' df aic'>
+                <div className=' df fdc mr5'>
+                </div>
+                <span
+                  style={{color: "red", cursor: 'pointer'}}
+                  onClick={() => {
+                    fetch(`${baseUrl}/datas/${_id}`, { method: 'DELETE' })
+                      .then((res) => res.json())
+                      .then((data) =>{
+                        console.log({ data })
+                      });
+                  }}
+                >
+                  BORRAR
+                </span>
+              </div>
+            </div>
+          ))}
         </div>
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </>
   );
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 'use client'
+
+// import { useEffect, useState } from "react";
+
+
+// const baseUrl = process.env.NEXT_PUBLIC_BACKEND_URL
+
+// export default function Home() {
+
+//   const [selectedProductId, setSelectedProductId] = useState()
+//   const [datas, setDatas] = useState([])
+//   console.log({setDatas})
+
+
+
+  
+//     const fetchDatas = () => {
+//       fetch(`${baseUrl}/datas`)
+//         .then((res) => res.json())
+//         .then(({ datas }) => {
+//           setDatas(datas)
+//         })
+//     }
+
+//     useEffect(() => {
+//       fetchDatas()
+//     }, []);
+
+//     console.log({ datas })
+
+//   return (
+//     <>
+//       <div className="container df jcsb">
+//         <div className="df fdc">
+//         <h2 style={{ margin: "0.3rem"}}>
+//                   <u>Crear nuevo producto</u>
+//                 </h2>
+//                   <form>
+//                       <input 
+//                         type="text"
+//                         name="name"
+//                         placeholder="ingrese algo"
+//                         // className='onone'
+//                         // value={product.name}
+//                         // onChange={handleChange}
+//                       />
+//                   </form> 
+//         </div>
+
+//         <div className='products-container'>
+//                 {datas.map(({ _id, megas}) =>(
+//                   <div 
+//                     onClick={() => setSelectedProductId(_id)}
+//                     key={_id}
+//                     className='shadow df aic jcsb p5 mb5 br5'
+//                     style={{
+//                       backgroundColor:
+//                         selectedProductId === _id ? 'lightblue' : 'white',
+//                         width: '100',
+
+//                     }}
+//                     >
+//                     <span style={{color: "red", cursor: 'pointer'}}>{megas}</span>
+//                     <div className=' df aic'>
+//                       <div className=' df fdc mr5'>
+//                         {/* <span style={{color: "red", cursor: 'pointer'}}>{eje}</span> */}
+//                         {/* <span>Stock: {stock}</span> */}
+
+
+//                       </div>
+//                     <span
+//                       style={{color: "red", cursor: 'pointer'}}
+//                       onClick={() => {
+//                         fetch(`${baseUrl}/datas/${_id}`, { method: 'DELETE' })
+//                           .then((res) => res.json())
+//                           .then((data) =>{
+//                             console.log({ data })
+//                           })
+//                         }}
+//                       >
+//                       BORRAR</span>
+//                     </div>
+//                   </div>
+//                 ))}
+//               </div>
+
+//       </div>
+//     </>
+//   );
+// }
