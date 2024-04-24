7be983f29b05d244dde7721ab10ceb4739b2f836
Asistenteplaneacion <apoyo.planeacion@fadp.edu.co>
2024-04-24 14:53:09 -0500

    cambios

--- app/page.js
+++ app/page.js
@@ -7,71 +7,25 @@ import styles from '../styles/home.css'
 const baseUrl = process.env.NEXT_PUBLIC_BACKEND_URL;
 
 export default function Home() {
-  const [selectedProductId, setSelectedProductId] = useState();
   const [datas, setDatas] = useState([]);
 
   const fetchDatas = () => {
-    fetch(`${baseUrl}/datas`)
+    fetch(`${baseUrl}`)
       .then((res) => res.json())
       .then(({ datas }) => {
         setDatas(datas);
       });
   };
 
-  useEffect(() => {
-    fetchDatas();
-  }, []);
-
-  console.log({ datas })
 
   return (
     <>
-      <div className="container df jcsb">
-        <div className="df fdc">
-          <h2 style={{ margin: "0.3rem"}}>
-            <u>Crear nuevo producto</u>
-          </h2>
-          <form>
-            <input 
-              type="text"
-              name="name"
-              placeholder="ingrese algo"
-            />
-          </form> 
+      <div onClick={fetchDatas}>
+        consultar
         </div>
-
-        <div className='products-container'>
-          {datas.map(({ _id, megas }) => (
-            <div 
-              onClick={() => setSelectedProductId(_id)}
-              key={_id}
-              className='shadow df aic jcsb p5 mb5 br5'
-              style={{
-                backgroundColor:
-                  selectedProductId === _id ? 'lightblue' : 'white',
-                width: '100',
-              }}
-            >
-              <span>{megas}</span>
-              <div className=' df aic'>
-                <div className=' df fdc mr5'>
-                </div>
-                <span
-                  style={{color: "red", cursor: 'pointer'}}
-                  onClick={() => {
-                    fetch(`${baseUrl}/datas/${_id}`, { method: 'DELETE' })
-                      .then((res) => res.json())
-                      .then((data) =>{
-                        console.log({ data })
-                      });
-                  }}
-                >
-                  BORRAR
-                </span>
-              </div>
-            </div>
-          ))}
-        </div>
+      
+      <div>
+        {datas}
       </div>
     </>
   );
